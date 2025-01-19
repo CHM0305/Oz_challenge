297fc3be994d1de5221a0086b2a52807a95eb681
Choi_Hye_Min <chym0305@naver.com>
2025-01-19 19:41:40 +0900

    🍀장고 수업 1일차🍀

---
+++ django/1일차/urls.py
@@ -0,0 +1,87 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.shortcuts import render,redirect
+from django.urls import path
+from django.http import HttpResponse, Http404
+
+song_list=[
+    {'title':'내가 제일 잘나가유','singer':'2NE1'},
+    {'title': '심장공격', 'singer': '2PM'},
+    {'title':'삐까뻔쩍','singer':'걸스데이'},
+    {'title': 'ABCD', 'singer': '레인보우'}
+]
+
+
+def main(request):
+    return HttpResponse("Hello World!")
+def book_list(request):
+    # book_text= ''
+    # for i in range(0,10):
+    #     book_text += f'book {i}<br>'
+
+    return render(request,'book.html', {'range':range(0,10)})
+def book(request,num1): #받는 변수명 꼭 통일해주기
+    return render(request,'book_detail.html', {'num1':num1})
+
+def language(request, lang):
+    return HttpResponse(f'<h1>{lang}언어 페이지입니다.</h1>')
+
+def songs(request):
+    # song_titles = [f'<a href="/song/{index}">{song["title"]}</a><br>'
+    #                for index,song in enumerate(song_list)
+    #                ]
+    return render(request, 'songs.html', {'song_list':song_list})
+
+def song_detail(request, num2):
+    #오류를 제대로 설명하기 위한 if문/에러를 지정해주지 않으면 무조건 서버측 에러 500번대가 나타남.
+    if num2 > len(song_list)-1:
+        raise Http404
+    #song=song_list[num2]
+    context= {'song_list':song_list,'num2':num2}
+    return render(request,'song.html',context)
+
+def gugu(request,num):
+    # if num < 2 :
+    #     #1. 값 지정 : 0,1단은 계산해도 값이 0과 1이기 때문에, 무조건 숫자를 지정해둔다.
+    #     #2. redirect 주소 지정 함수 터미널에는 302 로그가 뜸.
+    #     return redirect('/gugu/2')
+
+    context={
+        'num':num,
+        #html에 widthratio 넓이 구하는
+        #함수 사용할때
+        'results':[num*i for i in range(1,10)]
+        # 'range': range(1, 10)
+    }
+
+    return render(request,'gugudan.html',context)
+
+
+
+#0번부터 시작한다....!!!!
+urlpatterns = [
+    path('',main),
+    path('book_list/', book_list),
+    path("book_list/<int:num1>/", book),  #받는 변수명 꼭 통일해주기
+    path("book_list/<str:lang>/",language),
+    path('songs/',songs),
+    path('songs/<int:num2>/', song_detail),
+    path('gugu/<int:num>',gugu),
+
+
+]
